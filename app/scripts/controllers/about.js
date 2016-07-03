5a0b01c654604f0fdbe0449f999b40a9aeb3f809
kmhosny <kmhosny@gmail.com>
2016-07-03 13:20:16 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chatappApp
+ */
+angular.module('chatappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chatappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the chatappApp
+ */
+angular.module('chatappApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
