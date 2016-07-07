65db53daab1d6793018a04abe9d3ba49374ecf63
kmhosny <kmhosny@gmail.com>
2016-07-07 04:33:52 +0200

    chat module is working now, ordering needs to fixed and UI work left

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
@@ -37,3 +37,29 @@ angular
         redirectTo: '/'
       });
   });
+
+//User class defining the User properties
+var User = function(username,isOnline,id){
+  this.username = username;
+  this.id=id;
+  this.isOnline=isOnline;
+};
+
+//Conversation class defining the conversation properties
+var Conversation = function(convId,participants,isUnread,lastTimeStamp,name){
+  this.convId = convId;
+  this.participants=participants;
+  this.isUnread = isUnread;
+  this.lastTimeStamp = lastTimeStamp;
+  this.name = name
+};
+
+//Message class defining the message properties
+var Message = function(msgId,convId,sender,timeStamp,text)
+{
+  this.msgId = msgId;
+  this.convId = convId;
+  this.sender = sender;
+  this.timeStamp = timeStamp;
+  this.text = text;
+};
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,44 +8,35 @@
  * Controller of the chatappApp
  */
 angular.module('chatappApp')
-  .controller('loginCtrl', ['$scope','usersStorage','$window',function ($scope, usersStorage,$window) {
+  .controller('loginCtrl', ['$scope','usersStorage','$window','loggedInUser','$location',function ($scope, usersStorage,$window,loggedInUser,$location) {
     var ctrl = this;
     ctrl.userName = "";
-    ctrl.lastChatId = usersStorage.lastChatId;
-    ctrl.currentUser = undefined;
+    ctrl.currentUser = null;
     ctrl.signIn = function()
     {
-      $window.alert(ctrl.userName);
       var users = usersStorage.get();
       var searchUser = users.find(function(u){
-        return u.name == ctrl.userName;
+        return u.username == ctrl.userName;
       });
       if(searchUser == undefined)
       {
-        var user  = {name:ctrl.userName, isOnline:true};
-        usersStorage.insert(user);
-        ctrl.currentUser = user;
-        $window.alert("created new user");
+        ctrl.currentUser=usersStorage.insert(ctrl.userName,true);
+        console.log("created new user");
+        loggedInUser.set(ctrl.currentUser);
+        $location.path("/main");
         return;//create new user
       }
       if(searchUser.isOnline)
         {
           //error msg, already signed in
-          $window.alert("already online");
+          console.log("already online");
           return;
         }
       searchUser.isOnline = true;
       ctrl.currentUser = searchUser;
       usersStorage.update(searchUser);
-    };
-
-    $window.onbeforeunload= function(){
-      $window.alert("b2fl");
-      console.log(ctrl.currentUser);
-      if(ctrl.currentUser != undefined){
-        console.log(ctrl.currentUser);
-        ctrl.currentUser.isOnline = false;
-        usersStorage.update(ctrl.currentUser);
-      }
+      loggedInUser.set(ctrl.currentUser);
+      console.log("successfully signed in");
+      $location.path("/main");
     };
   }]);
